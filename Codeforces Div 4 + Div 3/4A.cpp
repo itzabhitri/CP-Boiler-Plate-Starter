e162086222fdbd87ac5a3db76b64f4192b8780ee
itzabhitri <itzabhitri@gmail.com>
2025-01-20 22:59:44 +0530

    new file:   Codeforces Div 4 + Div 3/4A.cpp

---
+++ Codeforces Div 4 + Div 3/4A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  int int_no;
+  cin >> int_no;
+  if(int_no >= 4 && int_no % 2 == 0)
+  cout << "YES";
+  else cout << "NO";
+  return 0;
+}
