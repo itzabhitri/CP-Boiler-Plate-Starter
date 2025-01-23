4231a08cd4acfce16e395740d10cd23cd4d6585a
itzabhitri <itzabhitri@gmail.com>
2025-01-23 23:18:15 +0530

    modified:   .vscode/settings.json

--- AtCoder ABCs/A's/380A.cpp
+++ AtCoder ABCs/A's/380A.cpp
@@ -7,6 +7,8 @@ int main() {
 
     int cnt1 = 0, cnt2 = 0, cnt3 = 0;
 
+// for (const char& c : N)
+ // for (char& c : N)
     for (char c : N) { // Loop through each character of the string
         if (c == '1') cnt1++; // Count occurrences of '1'
         else if (c == '2') cnt2++; // Count occurrences of '2'
@@ -21,6 +23,27 @@ int main() {
 
     return 0;
 }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a=0, b=0, c=0; char s[10];
+//      cin >> s;
+//     for(int i = 0; i < 6; ++i){
+//         if(s[i] == '1') ++a;
+//         if(s[i] == '2') ++b;
+//         if(s[i] == '3') ++c;
+//     }
+//     if(a == 1 && b == 2 && c == 3)
+//     cout << "Yes" <<endl;
+//     else cout << "No" <<endl;
+// }
+
+
+
+
   
   // sort(arr, arr+7);
   // cnt1 = arr[0];
@@ -96,19 +119,35 @@ int main() {
 
 
 
+// m_99 
+// #include <stdio.h>
+// #include <atcoder/all>
+// #include <bits/stdc++.h>
+// using namespace std;
+// using namespace atcoder;
+// using mint = modint998244353;
+// #define rep(i,n) for (int i = 0; i < (n); ++i)
+// #define Inf32 1000000001
+// #define Inf64 1000000000000000000LL
+
+// int main(){
+	
+// 	string s;
+// 	cin>>s;
+// 	vector<int> c(10);
+// 	rep(i,s.size())c[s[i]-'0']++;
+// 	if(c[1]==1&&c[2]==2&&c[3]==3)cout<<"Yes"<<endl;
+// 	else cout<<"No"<<endl;
+	
+// 	return 0;
+// }
+
+
+
+
 
 // https://atcoder.jp/contests/abc380/submissions/59825163
-// shiomusubi496's code
-// 1005 lines of code above this:
-// #line 2 "main.cpp"
-
-// using namespace std;
-
-// int main() {
-//     string S; scan >> S;
-//     sort(all(S));
-//     prints(S == "122333" ? "Yes" : "No");
-// }
+// shiomusubi496's code 1010 lines of code
 
 
 
